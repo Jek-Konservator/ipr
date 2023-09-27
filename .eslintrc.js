fbe65a9bbcff5f6289f847115a9934bc9ed5b5be
Alekseev_Roman <roma_alieksieiev.02@mail.ru>
2023-09-27 18:44:51 +0300

    IPR - настройка окружения

---
+++ .eslintrc.js
@@ -0,0 +1,81 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+  plugins: ['babel', 'import', 'jsx-a11y', 'react', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 6,
+    requireConfigFile: false,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': 'off',
+
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0,
+
+    'max-len': ['error', 500, 2, { ignoreUrls: true }],
+    'no-console': 'error',
+    'no-alert': 'error',
+
+    'no-param-reassign': 'off',
+    radix: 'off',
+
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/prefer-default-export': 'off',
+    'prefer-destructuring': 'off',
+
+    'react/no-find-dom-node': 'off',
+    'react/no-did-mount-set-state': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        required: {
+          every: ['id'],
+        },
+      },
+    ],
+
+    'prettier/prettier': ['error'],
+  },
+};
